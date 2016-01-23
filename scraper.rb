d4765ba48951dbdd984fde72c1d5d9120121437b
Tony Bowden <tony@tmtm.com>
2016-01-23 12:25:41 +0000

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,10 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'wikidata/fetcher'
+
+names = WikiData::Category.new('Thể loại:Đại biểu Quốc hội Việt Nam khóa 13', 'vi').member_titles
+EveryPolitician::Wikidata.scrape_wikidata(names: { vi: names }, output: false)
+warn EveryPolitician::Wikidata.notify_rebuilder
+
