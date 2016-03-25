ad436ea047cba7a501a452eec20adea1a0ac9ad2
Tony Bowden <tony@tmtm.com>
2016-03-25 11:56:49 +0000

    Use webhook instead of notifier

--- scraper.rb
+++ scraper.rb
@@ -6,5 +6,3 @@ require 'wikidata/fetcher'
 
 names = WikiData::Category.new('Thể loại:Đại biểu Quốc hội Việt Nam khóa 13', 'vi').member_titles
 EveryPolitician::Wikidata.scrape_wikidata(names: { vi: names }, output: false)
-warn EveryPolitician::Wikidata.notify_rebuilder
-
