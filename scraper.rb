25eeb32acb94b1142d849525a34d06323a79b589
Tony Bowden <tony@tmtm.com>
2016-11-26 10:40:21 +0000

    Replace WDQ with SPARQL

--- scraper.rb
+++ scraper.rb
@@ -5,6 +5,9 @@ require 'scraperwiki'
 require 'wikidata/fetcher'
 
 names = WikiData::Category.new('Thể loại:Đại biểu Quốc hội Việt Nam khóa 13', 'vi').member_titles
-ids = EveryPolitician::Wikidata.wdq('claim[39:17593571]')
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { vi: names }, output: false)
+sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q%s . }'
+ids = EveryPolitician::Wikidata.sparql(sparq % 17593571)
+raise "No ids" if ids.empty?
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { vi: names })
